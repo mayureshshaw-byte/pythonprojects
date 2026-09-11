16edd799523d215a9130b167b43f21b46bafc264
mayureshshaw-byte <mayureshshaw@gmail.com>
2026-09-11 12:52:01 +0530

    Add files via upload
    rock paper scissors game

---
+++ sps.py
@@ -0,0 +1,29 @@
+import random
+
+choice = ["rock", "paper", "scissor"]
+
+computer = random.choice(choice)
+
+user = input("Enter rock, paper or scissor: ")
+
+print("You chose:", user)
+print("Computer chose:", computer)
+
+if user == "scissor" and computer == "rock":
+    print("computer wins")
+elif user == "rock" and computer == "paper":
+    print ("computer wins")
+elif user == "paper" and computer == "scissor":
+    print("computer wins")
+elif user == "rock" and computer == "scissor":
+    print("You win!")
+
+elif user == "paper" and computer == "rock":
+    print("You win!")
+
+elif user == "scissor" and computer == "paper":
+    print("You win!")
+elif user == computer:
+    print("draw")    
+else :
+    print("invalid input")
